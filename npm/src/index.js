2b0aea573bdbce74a6b276c46afe6f70e91c3905
jgaffuri <jgaf@proton.me>
2023-07-02 11:06:18 +0200

    a

--- npm/src/index.js
+++ npm/src/index.js
@@ -8,9 +8,20 @@
 //https://github.com/topojson/topojson-server/tree/master
 
 
+/*
+install, uninstall locally
+https://github.com/nodejs/examples/blob/main/cli/commander/fake-names-generator/README.md#as-a-local-project
+
+sudo npm i -g
+# this will install the current working directory as a global module.
+
+gridtiler
+# run the CLI
+sudo npm uninstall -g
+*/
+
 
 export const hwfun = () => {
     console.log("hello world !")
 }
-
 
