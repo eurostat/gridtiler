8aa45bf10bca67278baa270afa45561e98cd14f3
jgaffuri <jgaf@proton.me>
2023-07-02 10:51:01 +0200

    a

--- npm/src/index.js
+++ npm/src/index.js
@@ -1,5 +1,14 @@
 
 
+//see
+//https://www.npmjs.com/package/commander
+
+//examples:
+//https://github.com/nodejs/examples/tree/main/cli/commander/fake-names-generator
+//https://github.com/topojson/topojson-server/tree/master
+
+
+
 export const hwfun = () => {
     console.log("hello world !")
 }
