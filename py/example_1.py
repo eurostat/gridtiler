06898e444ee64e1d975db1ffbd1b066c5238604f
jgaffuri <jgaf@proton.me>
2024-05-31 22:55:32 +0200

    a

--- py/example_1.py
+++ py/example_1.py
@@ -13,15 +13,25 @@ def cell_transformation(c):
     del c['GRD_ID']
     del c['CNTR_ID']
 
-"""
 print("Transformation")
-gridtiler.grid_transformation("assets/pop_5000m.csv", cell_transformation, "tmp/pop_5000m_prep.csv")
+gridtiler.grid_transformation("assets/pop_5000m.csv", cell_transformation, "tmp/pop_5000.csv")
+
+
 
 print("Aggregation to 10 000m")
-gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_10000m_prep.csv", 2, 0)
+gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_10000.csv", 2, 0)
 print("Aggregation to 20 000m")
-gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_20000m_prep.csv", 4, 0)
+gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_20000.csv", 4, 0)
 print("Aggregation to 50 000m")
-gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_50000m_prep.csv", 10, 0)
-"""
+gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_50000.csv", 10, 0)
 
+
+
+print("Tiling 5000m")
+gridtiler.grid_tiling("tmp/pop_5000.csv", "tmp/5000", 5000)
+print("Tiling 10 000m")
+gridtiler.grid_tiling("tmp/pop_10000.csv", "tmp/10000", 10000)
+print("Tiling 20 000m")
+gridtiler.grid_tiling("tmp/pop_20000.csv", "tmp/20000", 20000)
+print("Tiling 50 000m")
+gridtiler.grid_tiling("tmp/pop_50000.csv", "tmp/50000", 50000)
