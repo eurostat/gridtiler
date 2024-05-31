1042ee2a0a1d4fcbd7738447dc50cc1bfbc7fea3
jgaffuri <jgaf@proton.me>
2024-05-31 22:43:11 +0200

    a

--- py/example_1.py
+++ py/example_1.py
@@ -1,11 +1,16 @@
 from pygridmap import gridtiler
 
 
-def transfo(c):
-    c["x"] = 0
-    c["y"] = 0
-    print(c)
-    return
+#cell transformation function
+def cell_transformation(c):
+    #extract x and y from grid cell code
+    a = c['GRD_ID'].split("N")[1].split("E")
+    c["x"] = int(a[1])
+    c["y"] = int(a[0])
+    #delete unecessary data
+    del c['GRD_ID']
+    del c['CNTR_ID']
 
-gridtiler.grid_transformation("assets/pop_5000m.csv", transfo, "tmp/pop_5000m_prep.csv")
+print("Transformation")
+gridtiler.grid_transformation("assets/pop_5000m.csv", cell_transformation, "tmp/pop_5000m_prep.csv")
 
