59df442c6a6fbaef35f3de5f1dc25c708501d051
jgaffuri <jgaf@proton.me>
2024-05-31 22:50:54 +0200

    a

--- py/example_1.py
+++ py/example_1.py
@@ -13,9 +13,9 @@ def cell_transformation(c):
     del c['GRD_ID']
     del c['CNTR_ID']
 
-
-#print("Transformation")
-#gridtiler.grid_transformation("assets/pop_5000m.csv", cell_transformation, "tmp/pop_5000m_prep.csv")
+"""
+print("Transformation")
+gridtiler.grid_transformation("assets/pop_5000m.csv", cell_transformation, "tmp/pop_5000m_prep.csv")
 
 print("Aggregation to 10 000m")
 gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_10000m_prep.csv", 2, 0)
@@ -23,3 +23,5 @@ print("Aggregation to 20 000m")
 gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_20000m_prep.csv", 4, 0)
 print("Aggregation to 50 000m")
 gridtiler.grid_aggregation("tmp/pop_5000m_prep.csv", 5000, "tmp/pop_50000m_prep.csv", 10, 0)
+"""
+
