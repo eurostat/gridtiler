d9917294a635ef24aaad9dfbb762471d9165ee34
jgaffuri <jgaf@proton.me>
2024-06-01 18:53:31 +0200

    a

--- py/gt.py
+++ py/gt.py
@@ -215,16 +215,10 @@ def tiling_raster(in_raster_file, band_labels, output_folder, tile_size_cell=128
     r2 = resolution/2
     for i, label in enumerate(band_labels):
         data = raster.read(i+1)
-        def fun(xG,yG):
-            row, col = rowcol(transform, xG+r2, yG+r2)
-            #col = floor(width*(xG-x_min)/(x_max-x_min))
-            #row = floor(height*(yG-y_min)/(y_max-y_min))
-            #print(i,j)
-            #pixel_value = data[row, col]
-            #if i>=width: return None
-            #if j>=height: return None
-            #print(row, col)
-            #print(height, width)
+        def fun(x_cell,y_cell):
+            row, col = rowcol(transform, x_cell+r2, y_cell+r2)
+            if col>=width or col<0: return None
+            if row>=height or row <0: return None
             pixel_value = data[row,col]
             if pixel_value == no_data: return None
             return pixel_value
