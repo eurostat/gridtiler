db2addb2b275d8d806679b916183436158835393
jgaffuri <jgaf@proton.me>
2024-06-01 17:19:26 +0200

    a

--- py/gt.py
+++ py/gt.py
@@ -1,4 +1,7 @@
 import rasterio
+from math import ceil,floor
+
+#https://github.com/eurostat/JGiscoTools/blob/dev/modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gridProc/GridTiler2.java
 
 file_path = 'assets/LU001_LUXEMBOURG_UA2012_DHM_V020.tif'
 with rasterio.open(file_path) as src:
@@ -33,6 +36,19 @@ print(pixel_value)
 
 
 
-def tiling():
-    return
+#	public static void tile(String description, Map<String, ColummCalculator> values, Coordinate originPoint, Envelope envG, int resolutionG, int tileSizeNbCells, String crs, Format format, CompressionCodecName comp, String folderPath) {
+
+def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, x_max, y_max, tile_size_nb=128, crs="", format="csv", compression="snappy"):
+
+
+    #tile frame caracteristics
+    tileSizeGeo = resolution * tile_size_nb
+    tileMinX = floor( (x_min - x_or) / tileSizeGeo )
+    tileMinY = floor( (y_min - y_or) / tileSizeGeo )
+    tileMaxX = ceil( (x_max - x_or) / tileSizeGeo )
+    tileMaxY = ceil( (y_max - y_or) / tileSizeGeo )
+
+    #get keys
+    keys = values_calculator.keys()
+
 
