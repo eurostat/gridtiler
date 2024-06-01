d1db43a6029880b1bb0a61d15dcd1e6028f61f19
jgaffuri <jgaf@proton.me>
2024-06-01 17:53:03 +0200

    a

--- py/gt.py
+++ py/gt.py
@@ -1,7 +1,9 @@
 import rasterio
 from math import ceil,floor
-import csv
 import os
+import csv
+import json
+import pandas as pd
 
 #https://github.com/eurostat/JGiscoTools/blob/dev/modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gridProc/GridTiler2.java
 
@@ -40,18 +42,24 @@ print(pixel_value)
 
 #	public static void tile(String description, Map<String, ColummCalculator> values, Coordinate originPoint, Envelope envG, int resolutionG, int tileSizeNbCells, String crs, Format format, CompressionCodecName comp, String folderPath) {
 
-def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, x_max, y_max, tile_size_nb=128, crs="", format="csv", compression="snappy"):
+def tiling(values_calculator, resolution, output_folder, x_origin, y_origin, x_min, y_min, x_max, y_max, tile_size_cell=128, crs="", format="csv", compression="snappy"):
 
 
     #tile frame caracteristics
-    tileSizeGeo = resolution * tile_size_nb
-    tileMinX = floor( (x_min - x_or) / tileSizeGeo )
-    tileMinY = floor( (y_min - y_or) / tileSizeGeo )
-    tileMaxX = ceil( (x_max - x_or) / tileSizeGeo )
-    tileMaxY = ceil( (y_max - y_or) / tileSizeGeo )
+    tileSizeGeo = resolution * tile_size_cell
+    tileMinX = floor( (x_min - x_origin) / tileSizeGeo )
+    tileMinY = floor( (y_min - y_origin) / tileSizeGeo )
+    tileMaxX = ceil( (x_max - x_origin) / tileSizeGeo )
+    tileMaxY = ceil( (y_max - y_origin) / tileSizeGeo )
 
     #get keys
     keys = values_calculator.keys()
+
+    #minimum and maximum tile x,y, for info.json file
+    minTX=None
+    maxTX=None
+    minTY=None
+    maxTY=None
 
     #function to make cell template
     def make_cell():
@@ -60,15 +68,15 @@ def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, 
         return c
 
 	#TODO parallel ?
-    for tx in range(tileMinX, tileMaxX):
-        for ty in range(tileMinY, tileMaxY):
+    for xt in range(tileMinX, tileMaxX):
+        for yt in range(tileMinY, tileMaxY):
             #handle tile (tx,ty)
 
             #prepare tile cells
             cells = []
 
-            for xtc in range(0, tile_size_nb):
-                for ytc in range(0, tile_size_nb):
+            for xtc in range(0, tile_size_cell):
+                for ytc in range(0, tile_size_cell):
 
                     #make new cell
                     cell = None
@@ -76,8 +84,8 @@ def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, 
                     #get values
                     for k in keys:
                         #compute geo coordinate
-                        xG = x_or + tx * tileSizeGeo + xtc*resolution
-                        yG = y_or + ty * tileSizeGeo + ytc*resolution
+                        xG = x_origin + xt * tileSizeGeo + xtc*resolution
+                        yG = y_origin + yt * tileSizeGeo + ytc*resolution
 
                         if xG<x_min: continue
                         if xG>x_max: continue
@@ -105,8 +113,11 @@ def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, 
             #if no cell within tile, skip
             if len(cells) == 0: continue
 
-            #TODO store max/min x/y tile
-
+            #store extreme positions, for info.json file
+            if minTY == None or yt<minTY: minTY = yt
+            if maxTY == None or yt>maxTY: maxTY = yt
+            if minTX == None or xt<minTX: minTX = xt
+            if maxTX == None or xt>maxTX: maxTX = xt
 
             #remove column with all values null
             #check columns
@@ -129,11 +140,12 @@ def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, 
             headers.insert(1, "y")
 
             #create output folder if it does not already exists
-            fo = folder_out + "/" + tx + "/"
+            fo = output_folder + "/" + xt + "/"
             if not os.path.exists(fo): os.makedirs(fo)
 
             #save as CSV file
-            with open(fo + ty + ".csv", 'w', newline='') as csv_file:
+            cfp = fo + yt + ".csv"
+            with open(cfp, 'w', newline='') as csv_file:
                 writer = csv.writer(csv_file)
                 #write the header
                 writer.writerow(headers)
@@ -141,3 +153,36 @@ def tiling(values_calculator, resolution, folder_out, x_or, y_or, x_min, y_min, 
                 #write the cell rows
                 for c in cells:
                     writer.writerow(c)
+
+            if format == "csv": continue
+
+            #csv to parquet
+            #load csv file            
+            df = pd.read_csv(cfp)
+            #save as parquet            
+            df.to_parquet(fo + yt + ".parquet", engine='pyarrow', compression=compression, index=False)
+            #delete csv file
+            os.remove(cfp)
+
+
+    #write info.json
+    data = {
+        "dims": [],
+        "crs": crs,
+        "tileSizeCell": tile_size_cell,
+        "originPoint": {
+            "x": x_origin,
+            "y": y_origin
+        },
+        "resolutionGeo": resolution,
+        "tilingBounds": {
+            "yMin": minTY,
+            "yMax": maxTY,
+            "xMax": maxTX,
+            "xMin": minTX
+        }
+    }
+
+    with open(output_folder + '/info.json', 'w') as json_file:
+        json.dump(data, json_file, indent=3)
+
