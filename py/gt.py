2d3720531b8dd6c704bac602d739b27b6257e747
jgaffuri <jgaf@proton.me>
2024-06-02 16:08:08 +0200

    a

--- py/gt.py
+++ py/gt.py
@@ -198,21 +198,22 @@ def tiling_raster(rasters, output_folder, resolution, x_min, y_min, x_max, y_max
 
 
 print("start")
-tiling_raster(
-    {
-    "tcd_2012":{"file":'/home/juju/geodata/forest/in/forest_TCD_2012_100.tif', "band":1, 'no_data_values':[255,0]},
-    "tcd_2015":{"file":'/home/juju/geodata/forest/in/forest_TCD_2015_100.tif', "band":1, 'no_data_values':[255,0]},
-    "tcd_2018":{"file":'/home/juju/geodata/forest/in/forest_TCD_2018_100.tif', "band":1, 'no_data_values':[255,0]},
-    "dlt_2012":{"file":'/home/juju/geodata/forest/in/forest_DLT_2012_100.tif', "band":1, 'no_data_values':[255,0]},
-    "dlt_2015":{"file":'/home/juju/geodata/forest/in/forest_DLT_2015_100.tif', "band":1, 'no_data_values':[255,0]},
-    "dlt_2018":{"file":'/home/juju/geodata/forest/in/forest_DLT_2018_100.tif', "band":1, 'no_data_values':[255,0]}
- },
- "/home/juju/Bureau/forest/10000/",
- 10000,
-900000,
-900000,
-7400000,
-5500000,
-tile_size_cell=256,
-format="parquet"
-)
+for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
+    tiling_raster(
+        {
+        "tcd_2012":{"file":'/home/juju/geodata/forest/in/forest_TCD_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2015":{"file":'/home/juju/geodata/forest/in/forest_TCD_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2018":{"file":'/home/juju/geodata/forest/in/forest_TCD_2018_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2012":{"file":'/home/juju/geodata/forest/in/forest_DLT_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2015":{"file":'/home/juju/geodata/forest/in/forest_DLT_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2018":{"file":'/home/juju/geodata/forest/in/forest_DLT_2018_100.tif', "band":1, 'no_data_values':[255,0]}
+    },
+    "/home/juju/Bureau/forest/"+str(res)+"/",
+    res,
+    900000,
+    900000,
+    7400000,
+    5500000,
+    tile_size_cell=256,
+    format="parquet"
+    )
