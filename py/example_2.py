c2c544c37e47ede898eae59b242b07a3f47ad1aa
jgaffuri <jgaf@proton.me>
2024-06-02 17:33:06 +0200

    a

--- py/example_2.py
+++ py/example_2.py
@@ -1,6 +1,6 @@
-from pygridmap import gridtiler
+from pygridmap import gridtiler_raster
 
-gridtiler.tiling_raster(
+gridtiler_raster.tiling_raster(
     {"heigth":{"file":'assets/LU001_LUXEMBOURG_UA2012_DHM_V020.tif', "band":1, 'no_data_values':[255,0]} },
     "assets/lux_height/",
     10,
